324eaec8d7a8ba5539340f5cbb9f6e7eaa53d0a1
Jonathan Frascella <jonathan@highrise.digital>
2018-02-14 11:02:03 +0000

    Removed redundant code

--- gulpfile.js
+++ gulpfile.js
@@ -189,31 +189,6 @@ gulp.task( 'combinedJS', function() {
 });
 
 
- /**
-  * Task: `customJS`.
-  *
-  * Concatenate and uglify custom JS scripts.
-  *
-  * This task does the following:
-  *     1. Gets the source folder for JS custom files
-  *     2. Concatenates all the files and generates custom.js
-  *     3. Renames the JS file with suffix .min.js
-  *     4. Uglifes/Minifies the JS file and generates custom.min.js
-  */
-gulp.task( 'customJS', function() {
-	gulp.src( config.scripts_custom_src )
-	.pipe( concat( jsCustomFile + '.js' ) )
-	.pipe( lineec() ) // Consistent Line Endings for non UNIX systems.
-	.pipe( gulp.dest( jsCustomDestination ) )
-	.pipe( rename( {
-	  basename: jsCustomFile,
-	  suffix: '.min'
-	}))
-	.pipe( uglify() )
-	.pipe( lineec() ) // Consistent Line Endings for non UNIX systems.
-	.pipe( gulp.dest( jsCustomDestination ) )
-	.pipe( notify( { message: 'TASK: "customJs" Completed! 💯', onLast: true } ) );
-});
 
 
  /**
